c1394ee6924be95dd8261b0e79277a658cd85b17
JaiiR320 <jair3202@gmail.com>
2022-03-28 18:03:24 -0400

    Almost everything done

--- src/BufferPool.java
+++ src/BufferPool.java
@@ -1,9 +1,7 @@
-import java.sql.Blob;
 import java.util.ArrayList;
 
 public class BufferPool {
     ArrayList<Frame> frames = new ArrayList<>();
-    String message = "";
     int max;
     BufferPool(int max){
         this.max = max;
@@ -19,28 +17,48 @@ public class BufferPool {
      * @return
      */
     private int getFrame(int blockId) {
-        this.message = "";
         //case if the frame is in the buffer already
         for (Frame frame : frames) {
             if (frame.blockId == blockId) {
-                int out = frames.indexOf(frame);
-                message += "File " + blockId + " already in memory; located in frame " + (out+1);
-                return out;
-            }
-        }
-        //else bring block into buffer
-        for (Frame frame : frames) {
-            if ((frame.pinned == false && frame.dirty == false) || frame.blockId == -1) {
                 int index = frames.indexOf(frame);
-                frames.set(frames.indexOf(frame), new Frame("data/F" + blockId + ".txt"));
-                message += "brought file " + blockId + " from disk; placed in frame " + (index+1);
+                System.out.println("File " + blockId + " already in memory; located in frame " + (index+1));
                 return index;
             }
         }
+
+        //else bring block into buffer if there is empty space
+        for (Frame frame : frames) {
+            if (frame.blockId == -1) {
+                int index = frames.indexOf(frame);
+                frames.set(frames.indexOf(frame), new Frame("data/F" + blockId + ".txt"));
+                System.out.println("brought file " + blockId + " from disk; placed in frame " + (index+1));
+                return index;
+            }
+        }
+
+        //evict candidate to make space
+        for (Frame frame : frames) {
+            if (frame.pinned == false){
+                if(frame.dirty);//write
+                int prev = frame.blockId;
+                int index = frames.indexOf(frame);
+                frames.set(frames.indexOf(frame), new Frame("data/F" + blockId + ".txt"));
+                System.out.println("evicted file " + prev + " from frame " + (index+1));
+                System.out.println("placed " + blockId + " in frame " + (index+1));
+                return index;
+            }
+        }
+        //cannot put in system
         return -1;
     }
 
-    String get(int recordId){
+    /**
+     * Get the requested record, based off it's record ID
+     * @param recordId
+     * @return
+     */
+    void get(int recordId){
+        System.out.println("getting " + recordId);
         int blockId = 0;
         if(recordId % 100 == 0)
             blockId = recordId / 100;
@@ -50,13 +68,19 @@ public class BufferPool {
         int bufferIndex = this.getFrame(blockId);
         if(bufferIndex != -1){
             String out = frames.get(bufferIndex).record(recordId);
-            System.out.println(out + "; " + this.message);
-            return frames.get(bufferIndex).record(recordId);
+            System.out.println(out + '\n');
+            return;
         }
-        return "";
+        System.out.println("Process blocked, no space in buffer\n");
     }
 
+    /**
+     * Set the data of the corresponding record, set frame as dirty
+     * @param recordId
+     * @param data
+     */
     void set(int recordId, String data){
+        System.out.println("setting " + recordId);
         int blockId = 0;
         if(recordId % 100 == 0)
             blockId = recordId;
@@ -65,27 +89,59 @@ public class BufferPool {
 
         int bufferIndex = getFrame(blockId);
         if(bufferIndex == -1) {
-            System.out.println("error; " + this.message);
+            System.out.println("Process blocked, no space in buffer\n");
             return;
         }
         Frame f = frames.get(bufferIndex);
         f.records[(recordId % 100) - 1] = data;
         f.dirty = true;
-        System.out.println("write was successful; " + this.message);
+        System.out.println("write was successful\n");
     }
 
+    /**
+     * Pins the corresponding file in memory
+     * If the block is not in memory, attempt to bring it in before pinning
+     * @param blockId
+     */
     void pin(int blockId){
-        for (Frame frame : frames) {
-            if (frame.blockId == blockId) {
-                frame.pinned = true;
-            }
+        System.out.println("pinning " + blockId);
+        int index = getFrame(blockId);
+
+        if(index == -1){
+            System.out.println("Cannot bring frame into memory\n");
+            return;
         }
+
+        String out = "File " + blockId + " pinned in Frame " + (index+1);
+        if (frames.get(index).pinned) 
+            out += "; Already pinned";
+        else
+            out += "; Not already pinned";
+
+        frames.get(index).pinned = true;
+        System.out.println(out + "\n");
     }
+
+    /**
+     * Unpin the corresponding block in the buffer pool
+     * if the block is not in memory, do nothing
+     * @param blockId
+     */
     void unpin(int blockId){
+        System.out.println("unpinning " + blockId);
         for (Frame frame : frames) {
             if (frame.blockId == blockId) {
+                String out = "File " + blockId + " in Frame " + (frames.indexOf(frame)+1) + " unpinned; ";
+                if(frame.pinned)
+                    out += "Frame was not already unpinned";
+                else
+                    out += "Frame was already unpinned";
+                System.out.println(out+"\n");
                 frame.pinned = false;
+                return;
             }
         }
+        System.out.println("block " + blockId + " cannot be unpinned because it is not in memory\n");
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,51 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         BufferPool p = new BufferPool(Integer.valueOf(3));//args[1]));
+
+        p.set(430, "F05-Rec450, Jane Do, 10 Hill Rd, age020.");
+        //Output: Write was successful; Brought File 5 from disk; Placed in Frame 1
+        p.get(430);
+        //Output: F05-Rec450, Jane Do, 10 Hill Rd, age020.; File 5 already in memory; Located in Frame 1
+        p.get(20);
+        //Output: F01-Rec020, Name020, address020, age020.; Brought file 1 from disk; Placed in Frame 2
+        p.set(430, "F05-Rec450, John Do, 23 Lake Ln, age056.");
+        //Output: Write was successful; File 5 already in memory; Located in Frame 1
+        p.pin(5);
+        //Output: File 5 pinned in Frame 1; Not already pinned
+        p.unpin(3);
+        //Output: The corresponding block 3 cannot be unpinned because it is not in memory.
+        p.get(430);
+        //Output: F05-Rec450, John Do, 23 Lake Ln, age056.; File 5 already in memory; Located in Frame 1
+        p.pin(5);
+        //Output: File 5 pinned in Frame 1; Already pinned
+        p.get(646);
+        //Output: F07-Rec646, Name646, address646, age646.; Brought file 7 from disk; Placed in Frame 3
+        p.pin(3);
+        //Output: File 3 pinned in Frame 2; Not already pinned; Evicted file 1 from Frame 2 
+        p.set(10, "F01-Rec010, Tim Boe, 09 Deer Dr, age009.");
+        //Output: Write was successful; Brought File 1 from disk; Placed in Frame 3; Evicted file 7 from Frame 3
+        p.unpin(1);
+        //Output: File 1 in frame 3 is unpinned; Frame was already unpinned
+        p.get(355);
+        //Output: F04-Rec355, Name355, address355, age355.; Brought file 4 from disk; Placed in Frame 3; Evicted file 1 from frame 3
+        p.pin(2);
+        //Output: File 2 is pinned in Frame 3; Frame 3 was not already pinned; Evicted file 4 from frame 3
+        p.get(156);
+        //Output: F02-Rec156, Name156, address156, age156.; File 2 already in memory; Located in Frame 3
+        p.set(10, "F01-Rec010, No Work, 31 Hill St, age100.");
+        //Output: The corresponding block #1 cannot be  accessed from disk because the memory buffers are full; Write was unsuccessful 
+        p.pin(7);
+        //Output: The corresponding block 7 cannot be pinned because the memory buffers are full
+        p.get(10);
+        //Output: The corresponding block #1 cannot be accessed from disk because the memory buffers are full
+        p.unpin(3);
+        //Output: File 3 is unpinned in frame 2; Frame 2 was not already unpinned
+        p.unpin(2);
+        //Output: File 2 is unpinned in frame 3; Frame 3 was not already unpinned
+        p.get(10);
+        //Output: F01-Rec010, Tim Boe, 09 Deer Dr, age009.; Brought file 1 from disk; Placed in Frame 2; Evicted file 3 from frame 2
+        p.pin(6);
+        //Output: File 6 pinned in Frame 3; Not already pinned; Evicted file 2 from frame 3
         while(true){
             String[] com = scan.nextLine().split("\\ ", 3);
             switch (com[0]) {
